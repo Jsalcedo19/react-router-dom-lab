c60c90ba335fb827be774e67baf6fe3bf2b5151e
Jose Salcedo <Jsalcedo19@gmail.com>
2024-12-10 13:34:07 -0500

    Import mailbox components and Navbar in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
-// src/App.jsx
+import React from 'react';
+import './App.css';
+import './components/mailboxDetails';
+import './components/mailboxForm';
+import './components/mailboxList';
+import './components/Navbar';
+
+
 
 const App = () => {
   return <h1>Hello world!</h1>;
