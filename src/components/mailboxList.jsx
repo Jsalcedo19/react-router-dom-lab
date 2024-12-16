91a3433cfa979960cd0ec3a2463566734d97cc17
Jose Salcedo <Jsalcedo19@gmail.com>
2024-12-15 19:59:28 -0500

    Update MailboxList to accept mailboxes prop and render mailbox details with links

--- src/components/mailboxList.jsx
+++ src/components/mailboxList.jsx
@@ -1,12 +1,19 @@
 import { Link } from 'react-router-dom'
 
-const MailboxList = () => {
-
-
-
-  return (
-    <div>mailboxList</div>
-  )
+const MailboxList = ({ mailboxes }) => {
+  
+return (
+    <div>
+      {mailboxes.map(mailbox => (
+        <div key={mailboxes._id} className='mail-box'>
+          <Link to={`/mailboxes/${mailbox._id}`}>
+            <h2>Mailbox {mailbox._id}</h2>
+            <p>Boxholder: {mailbox.boxholder}</p>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default MailboxList
