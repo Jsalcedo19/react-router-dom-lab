94050b86bb1b696a485ce886851cb8a54e8f3681
Jose Salcedo <Jsalcedo19@gmail.com>
2024-12-15 20:06:04 -0500

    improved readability and structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,42 @@
-import { Route, Routes, } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import MailboxList from './components/MailboxList'; 
-import MailboxDetails from './components/MailboxDetails'; 
-import MailboxForm from './components/MailboxForm';
-import { useState } from 'react';
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import MailboxList from "./components/MailboxList";
+import MailboxDetails from "./components/MailboxDetails";
+import MailboxForm from "./components/MailboxForm";
+import { useState } from "react";
 
 const App = () => {
- const [mailboxes, setMailboxes] = useState([]);
+  const [mailboxes, setMailboxes] = useState([]);
 
-const addbox = (newMailbox) => { 
-  newMailbox.id = mailboxes.length + 1;
-  setMailboxes([...mailboxes, newMailbox]);
-};
+  const addbox = (newMailbox) => {
+    newMailbox.id = mailboxes.length + 1;
+    setMailboxes([...mailboxes, newMailbox]);
+  };
 
-
-return (
-  <div>
-    <Navbar />
-     <Routes>
-      <Route path="/" element={<main><h1>Post Office</h1></main>} />
-      <Route path="/mailboxes" element={<MailboxList mailboxes={mailboxes} />} />
-      <Route path="/new-mailbox" element={<MailboxForm addbox={addbox} />}/>
-      <Route path="/mailboxes/:id" element={<MailboxDetails mailboxes={mailboxes}/>} />
-    </Routes>
-
-</div>
-
-
-
-
-);
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <main>
+              <h1>Post Office</h1>
+            </main>
+          }
+        />
+        <Route
+          path="/mailboxes"
+          element={<MailboxList mailboxes={mailboxes} />}
+        />
+        <Route path="/new-mailbox" element={<MailboxForm addbox={addbox} />} />
+        <Route
+          path="/mailboxes/:id"
+          element={<MailboxDetails mailboxes={mailboxes} />}
+        />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
-
