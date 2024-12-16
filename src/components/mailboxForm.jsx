aea9d581d390075aee4deb1bd6cf7f9559ee6e53
Jose Salcedo <Jsalcedo19@gmail.com>
2024-12-15 19:43:19 -0500

    MailboxForm to accept addbox prop and update MailboxList to include routing

--- src/components/mailboxForm.jsx
+++ src/components/mailboxForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const MailboxForm = () => {
+const MailboxForm = (addbox) => {
 const [boxholder, setBoxholder] = useState('');
 const [boxsize, setBoxsize] = useState('');
 const navigate = useNavigate();
--- src/components/mailboxList.jsx
+++ src/components/mailboxList.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 const MailboxList = () => {
+
+
+
   return (
     <div>mailboxList</div>
   )
